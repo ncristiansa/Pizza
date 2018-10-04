e58befd14682c572098b09249acf044515eb988b
ncristiansa <csalinasandia@iesesteveterradas.cat>
2018-10-04 18:00:29 +0200

    formulario

--- Pizza.php
+++ Pizza.php
@@ -8,13 +8,16 @@
 	<?php
 		$formulario = "
 			<form method='post' action=''>
-				<select>
-					<option></option>
-					<option></option>
-					<option></option>
-				</select>
+				<input type='checkbox' name='massa' value='Masa'>Masa<br>
+				<input type='checkbox' name='tomate' value='Tomate'>Tomate<br>
+				<input type='checkbox' name='queso' value='Queso'>Queso<br>
+				<input type='checkbox' name='oregano' value='Oregano'>Oregano<br>
+				<input type='checkbox' name='bacon' value='Bacon'>Bacon<br>
+				<input type='submit' value='Enviar'></button>
 			</form>";
+			echo"<h3>Elige tu pedido.</h3>";<
 			echo"$formulario";
+
 	?>
 </body>
 </html>
