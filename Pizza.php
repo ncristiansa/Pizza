b05ace8bf2bf9f16ae73f8a3d8cb5de404d3649a
ncristiansa <csalinasandia@iesesteveterradas.cat>
2018-10-04 17:40:19 +0200

    archivo

---
+++ Pizza.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Pizza</title>
+</head>
+<body>
+	<?php
+		$formulario = "
+			<form method='post' action=''>
+				<select>
+					<option></option>
+					<option></option>
+					<option></option>
+				</select>
+			</form>";
+			echo"$formulario";
+	?>
+</body>
+</html>
